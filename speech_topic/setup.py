bec29cb159abfb53c69db9c5b29593d1c7cf736a
Yuki-okuma <83355831+Yuki-okuma@users.noreply.github.com>
2024-06-28 14:13:16 +0900

    [UPD] setup.py

--- speech_topic/setup.py
+++ speech_topic/setup.py
@@ -21,7 +21,7 @@ setup(
     entry_points={
         'console_scripts': [
             'recognition = speech_topic.recognition:main',
-            'synthesys = speech_topic.synthesys:main'
+            'synthesys = speech_topic.synthesys:main',
             'synthesys_mpg123 = speech_topic.synthesys_mpg123:main'
         ],
     },
